105df5a9a49a60dbad8031c621c2b3770f8b5fa4
imamiya-masaki <imamiyagit@gmail.com>
2022-01-07 07:42:58 +0900

    BJのボーナス計算をやり忘れていたので

--- main.py
+++ main.py
@@ -78,6 +78,9 @@ class montekarlo:
     N = self.tree[str(playerSumCards)+'-'+str(delerCards)+'-'+str(player1D)]['N']
     for key in ['HIT', 'STAY', 'DOUBLE']:
       target = self.tree[str(playerSumCards)+'-'+str(delerCards)+'-'+str(player1D)][key]
+      if targetVal >= 99999:
+        #targetValが初期値選定されたらそれを超えることはできないので、
+        break
       if target['n'] == 0:
         if not key  ==  'DOUBLE' or player1D == True:
           val = 99999
@@ -90,7 +93,7 @@ class montekarlo:
         targetVal = val
         targetKey = key
     return {'action': targetKey, 'state': str(playerSumCards)+'-'+str(delerCards)+'-'+str(player1D)}
-  def learning(self, actions: list, reward: int):
+  def simpleLearning(self, actions: list, reward: int):
     for info in actions:
       action = info['action']
       state = info['state']
@@ -100,6 +103,22 @@ class montekarlo:
       self.tree[state][action]['n'] += 1
       self.tree[state]['N'] += 1
     return
+  def learning(self, actions: list, reward: int):
+    self.simpleLearning(actions, reward) #まずシンプルラーニングはする
+    if actions[0]['state'] not in self.tree:
+      self.tree[actions[0]['state']] = createInitial()
+    for index in range(len(actions)-1):
+      preInfo = actions[index]
+      nextInfo = actions[index+1]
+      preAction = preInfo['action']
+      nextAction = nextInfo['action']
+      preState = preInfo['state']
+      nextState = nextInfo['state']
+      if nextState not in self.tree:
+        self.tree[nextState] = createInitial()
+      self.tree[preState][preAction]['val'] += (self.tree[nextState][nextAction]['val']/self.tree[nextState][nextAction]['n'])
+    return
+
   def output_learnData(self):
     return self.tree
 class Game:
@@ -144,7 +163,6 @@ class Game:
     str_lists: str[...] = [str(val) for val in lists]
     return '/'.join(str_lists), True
 
-    return ""
   def max(self, sumCard:str) -> int:
     cards = sumCard.split('/')
     return int(cards[len(cards) - 1]) #昇順なので最後に最大値がくる
@@ -169,18 +187,19 @@ class Game:
     player1Cards = []
     dealer = dealerClass()
     player1D = False #ダブルダウンしたかどうか
+    playerBJ = False #playerブラックジャック
     delerCards = []
     player1Cards,deck = self.pickAndInsertCards(deck, player1Cards, 2)
     delerCards,deck = self.pickAndInsertCards(deck, delerCards, 1)
     playerSum = self.ini_sum(player1Cards)
     delerSum = self.ini_sum(delerCards)
-    #playerの行動
     plyer1_select = [] #本質的にはdictの配列なんだけど、なぜか型宣言できない...
     if self.max(delerSum) == 21:
       # ディーラーが初期手札で21ならディーラーの勝ち
       return plyer1_select, -1
       #報酬を-1としているが、player1_selectがからなので影響しない~...と考えれるし、
       #インスランスをつけたくなってもいい感じにできそう
+    #playerの行動
     while True:
       get: Dict[str, str] = player1.play(playerSum, delerSum, player1D)
       plyer1_select.append(get)
@@ -194,6 +213,12 @@ class Game:
       if get['action'] == 'STAY' or result == 'LOSE' or player1D == True:
         #playerがstayを選択か、(バースト等して)負けかダブルダウンを選択したら、プレイヤーの行動は終了
         break
+    if result is not "LOSE" and self.max(playerSum) == 21:
+      #ディーラーが21ではなくて、playerが21なら
+      value = 1.5 #playerがBJの時の返りはボーナス付き
+      if player1D:
+        value *= 2
+      return plyer1_select, value
     #ディーラーの行動
     if result is not "LOSE":
       while True:
@@ -228,10 +253,13 @@ class Game:
 if __name__ == '__main__':
     player1Sum = 0 # player0の勝ち星　グラフよう
     logs = [] # playerのログをとる
+    nextLogs = [] #learning強化
     pres = []
     logs.append(player1Sum)
+    nextLogs.append(player1Sum)
     pres.append(player1Sum)
     player1 = montekarlo()
+    player2 = montekarlo()
     for i in range(1000):
       game = Game(1)
       player_select, result = game.playGame(player1)
@@ -239,16 +267,21 @@ if __name__ == '__main__':
     for i in range(50000):
       # 50000回繰り返す
       game = Game(1)
-      player_select, result = game.playGame(player1)
-      player1.learning(player_select, result*100)
+      player_select1, result1 = game.playGame(player1)
+      player1.simpleLearning(player_select1, result1*100)
+      player_select2, result2 = game.playGame(player2)
+      player2.learning(player_select2, result2*100)
     for i in range(1000):
       # 100回繰り返す
       game = Game(1)
-      player_select, result = game.playGame(player1)
-      logs.append(result+logs[len(logs)-1])
-      player1.learning(player_select, result*100)
-    x_1 = np.array([ i for i in range(len(logs)) ])
-    x_2 = np.array([ i for i in range(len(pres)) ])
-    pyplot.plot(x_1, np.array(logs), color='red')
-    pyplot.plot(x_2, np.array(pres), color='yellow')
+      player_select1, result1 = game.playGame(player1)
+      player_select2, result2 = game.playGame(player2)
+      logs.append(result1+logs[len(logs)-1])
+      nextLogs.append(result2+nextLogs[len(nextLogs)-1])
+    x_1 = np.array([ i for i in range(len(pres)) ])
+    x_2 = np.array([ i for i in range(len(logs)) ])
+    x_3 = np.array([ i for i in range(len(nextLogs)) ])
+    pyplot.plot(x_1, np.array(pres), color='yellow')
+    pyplot.plot(x_2, np.array(logs), color='red')
+    pyplot.plot(x_3, np.array(nextLogs), color='blue')
     pyplot.show()
